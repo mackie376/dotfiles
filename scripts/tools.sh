148f5f4998d11abb4acf15b6c4911b940bfd0453
Takashi Makimoto <mackie@beehive-dev.com>
2021-07-14 06:11:25 +0900

    Add configurations for gnupg

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -5,3 +5,12 @@ mkdir -p "${HOME}/.ghq"
 mkdir -p "${HOME}/Projects"
 link_files "${DOTFILES_ETC_HOME}/git" "${XDG_CONFIG_HOME}/git"
 show_ok git
+
+## gnupg
+if is_macos; then
+  link_files "${DOTFILES_ETC_HOME}/gnupg" "${HOME}/.gnupg"
+  chmod og-rx "${HOME}/.gnupg"
+  show_ok gnupg
+else
+  show_na gnupg
+fi
