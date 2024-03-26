d69e285204033c4800e9c1621fba47d8a9c43793
Takashi Makimoto <mackie@beehive-dev.com>
2024-03-26 20:15:24 +0900

    add first version of bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# --------------------------------------------------------------------------
+
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_CACHE_HOME="${HOME}/.cache"
+XDG_DATA_HOME="${HOME}/.local/share"
+XDG_STATE_HOME="${HOME}/.local/state"
+XDG_BIN_HOME="${HOME}/.local/bin"
+
+PROJECTS_DIR="${HOME}/Projects"
+
+# --------------------------------------------------------------------------
+
+mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_CACHE_HOME"
+mkdir -p "$XDG_DATA_HOME"
+mkdir -p "$XDG_STATE_HOME"
+mkdir -p "$XDG_BIN_HOME"
+mkdir -p "$PROJECTS_DIR"
+
+# --------------------------------------------------------------------------
+
+if [[ "$(uname -s)" =~ 'Linux' ]]; then
+  if ! which curl &> /dev/null; then
+    sudo apt curl
+  fi
+  if ! which git &> /dev/null; then
+    sudo apt git
+  fi
+fi
+
+# --------------------------------------------------------------------------
+
+if [[ "$(uname -s)" =~ 'Darwin' ]]; then
+  if ! xcode-select --print-path &> /dev/null; then
+    xcode-select --install &> /dev/null
+    until xcode-select --print-path &> /dev/null; do
+      sleep 5
+    done
+  fi
+  if [[ ! -x /opt/homebrew/bin/brew ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# --------------------------------------------------------------------------
+
+if [[ ! -x "${XDG_BIN_HOME}/chezmoi" ]]; then
+  sh -c "$(curl -fsSL get.chezmoi.io)" -- -b "$XDG_BIN_HOME"
+fi
+
+if [[ ! -x "${XDG_BIN_HOME}/mise" ]]; then
+  curl https://mise.run | sh
+fi
+
+export PATH="$XDG_BIN_HOME:$PATH"
+
+# --------------------------------------------------------------------------
+
+echo -e "\nDone.\n"
