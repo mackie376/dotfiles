3c25cf4831c5c2acecaed9884a89595d30ff92ff
Takashi Makimoto <mackie@beehive-dev.com>
2024-05-20 19:10:24 +0900

    apt を使ったインストールスクリプトを追加

---
+++ run_once_install_with_apt.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt install -y \
+  automake \
+  bison \
+  build-essential \
+  btop \
+  cmake \
+  curl \
+  git \
+  git-lfs \
+  gnupg \
+  jq \
+  libbz2-dev \
+  libevent-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  liblzma-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  make \
+  neofetch \
+  openssl \
+  pkg-config \
+  preload \
+  rsync \
+  tk-dev \
+  trash-cli \
+  unzip \
+  vim \
+  xz-utils \
+  zlib1g-dev \
+  wget \
+  zsh
